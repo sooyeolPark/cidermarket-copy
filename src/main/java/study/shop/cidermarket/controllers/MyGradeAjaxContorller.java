08ab2ab30e8b7064f009964688225de3426e001d
donghun <hoonyhoeny@gmail.com>
2021-03-01 00:57:03 +0900

    [Revise] 이동훈 Mygrade 에러수정

--- src/main/java/study/shop/cidermarket/controllers/MyGradeAjaxContorller.java
+++ src/main/java/study/shop/cidermarket/controllers/MyGradeAjaxContorller.java
@@ -91,21 +91,10 @@ public class MyGradeAjaxContorller {
 			return webHelper.redirect(null, e.getLocalizedMessage());
 		}
 
-		Record input_03 = new Record();
-		input_03.setMembno(myNum);
-		Record output = null;
-
-		try {
-			// 데이터 조회
-			output = recordService.getUserInfo(input_03);
-		} catch (Exception e) {
-			return webHelper.redirect(null, e.getLocalizedMessage());
-		}
-
+		
 		// 모델 객체에 담기
 		model.addAttribute("output_sell", output_sell);
 		model.addAttribute("output_buy", output_buy);
-		model.addAttribute("output", output);
 		model.addAttribute("user", user);
 
 		return new ModelAndView("user/mystore_mygrade");
